e5d2c13bb405f8ad9c0a9db54e5b16761dfab171
ddr <mrdrifter123@outlook.com>
2024-07-11 18:43:08 +1000

    added class "grid-item" to each div

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ const container = document.querySelector(".container");
 // create divs
 for(i=0; i<16*16; i++){
     let newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "grid-item");
     container.appendChild(newDiv);
 }
