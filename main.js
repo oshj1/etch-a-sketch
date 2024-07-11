e8ddd5643ac688278419fdc5d268369421875573
ddr <mrdrifter123@outlook.com>
2024-07-11 19:23:58 +1000

    added mouseenter event listener which changes the color of any grid where the mouse enters

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const container = document.querySelector(".container");
 const Rows = 16;
 const ItemsPerRow = 16;
 
+
 function createGrid(){
     for (let i=0; i<Rows; i++){
         let newRow = document.createElement("div");
@@ -11,6 +12,11 @@ function createGrid(){
         for(let i=0; i<ItemsPerRow; i++){
             let newGridItem = document.createElement("div");
             newGridItem.setAttribute("class", "grid-item");
+
+            newGridItem.addEventListener("mouseenter", function(){
+                newGridItem.style.backgroundColor = "green";
+            });
+
             newRow.appendChild(newGridItem);
         }
     }
